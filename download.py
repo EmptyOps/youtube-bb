8a5c8b6335006e2d0059d1dc02ab1c164a3b6477
hiren <hiren@emptyops.com>
2020-03-19 17:51:53 +0530

    some memory optimizations added to make sure that process can run with light memory footprint

--- download.py
+++ download.py
@@ -29,12 +29,33 @@ def parse_and_sched(dl_dir='videos',num_threads=4,dl_cls_by_filter=-1,offset_min
   # Make the download directory if it doesn't already exist
   check_call(['mkdir', '-p', dl_dir])
 
+  #
+  read_csv_with_alternate_reader=True
+
   # For each of the four datasets
+  rec_ind_glb = 0
   rec_ind = -1
   for d_set in youtube_bb.d_sets:
 
-    annotations,clips,vids = youtube_bb.parse_annotations(d_set,dl_dir,dl_cls_by_filter)
-    rec_ind = youtube_bb.sched_downloads(d_set,dl_dir,num_threads,vids,rec_ind,offset_min,offset_max,FREE_SPACE_LIMIT)
+    offset_min_tmp = offset_min
+    offset_max_tmp = offset_max
+
+    if read_csv_with_alternate_reader:
+      offset_min_tmp -= rec_ind_glb
+      offset_max_tmp -= rec_ind_glb
+
+    annotations,clips,vids,rig = youtube_bb.parse_annotations(d_set,dl_dir,rec_ind,offset_min_tmp,offset_max_tmp,dl_cls_by_filter,read_csv_with_alternate_reader=read_csv_with_alternate_reader)
+    
+    if read_csv_with_alternate_reader:
+      rec_ind_glb += rig
+    
+      rec_ind = -1
+      offset_min_tmp = 0
+      offset_max_tmp = offset_max - offset_min
+
+    rec_ind = youtube_bb.sched_downloads(d_set,dl_dir,num_threads,vids,rec_ind,offset_min_tmp,offset_max_tmp,FREE_SPACE_LIMIT)
+
+  print("Processing finished of all datasets")
 
 if __name__ == '__main__':
 
@@ -42,13 +63,13 @@ if __name__ == '__main__':
           "Usage: python download.py [VIDEO_DIR] [NUM_THREADS] [CLASS_FILTER(optional)] [OFFSET_MIN(optional, offset work with accumulated sum of all 4 datasets)] [OFFSET_MAX(optional, offset work with accumulated sum of all 4 datasets)] [FREE_SPACE_LIMIT(optional, specify in MBs the number of MBs worth of free space that you want to retain on the VIDEO_DIR mount so that it stops when free space go below specified limit)]"
   # Use the directory `videos` in the current working directory by
   # default, or a directory specified on the command line.
-if len(sys.argv) >= 7:
-  parse_and_sched(sys.argv[1],int(sys.argv[2]),int(sys.argv[3]),int(sys.argv[4]),int(sys.argv[5]),int(sys.argv[6]))
-elif len(sys.argv) >= 6:
-  parse_and_sched(sys.argv[1],int(sys.argv[2]),int(sys.argv[3]),int(sys.argv[4]),int(sys.argv[5]))
-elif len(sys.argv) >= 5:
-  parse_and_sched(sys.argv[1],int(sys.argv[2]),int(sys.argv[3]),int(sys.argv[4]))
-elif len(sys.argv) >= 4:
-  parse_and_sched(sys.argv[1],int(sys.argv[2]),int(sys.argv[3]))
-else:  
-  parse_and_sched(sys.argv[1],int(sys.argv[2]))
+  if len(sys.argv) >= 7:
+    parse_and_sched(sys.argv[1],int(sys.argv[2]),int(sys.argv[3]),int(sys.argv[4]),int(sys.argv[5]),int(sys.argv[6]))
+  elif len(sys.argv) >= 6:
+    parse_and_sched(sys.argv[1],int(sys.argv[2]),int(sys.argv[3]),int(sys.argv[4]),int(sys.argv[5]))
+  elif len(sys.argv) >= 5:
+    parse_and_sched(sys.argv[1],int(sys.argv[2]),int(sys.argv[3]),int(sys.argv[4]))
+  elif len(sys.argv) >= 4:
+    parse_and_sched(sys.argv[1],int(sys.argv[2]),int(sys.argv[3]))
+  else:  
+    parse_and_sched(sys.argv[1],int(sys.argv[2]))
--- youtube_bb.py
+++ youtube_bb.py
@@ -24,6 +24,9 @@ import io
 import sys
 import csv
 
+import pandas as pd
+import pickle
+
 # Debug flag. Set this to true if you would like to see ffmpeg errors
 debug = False
 
@@ -187,7 +190,19 @@ def dl_and_cut(vid):
 
 
 # Parse the annotation csv file and schedule downloads and cuts
-def parse_annotations(d_set,dl_dir,dl_cls_by_filter=-1):
+# read_csv_with_alternate_reader: memory savvy reader
+def parse_annotations(d_set,dl_dir,rec_ind,offset_min,offset_max,dl_cls_by_filter=-1, read_csv_with_alternate_reader=False):
+
+  rig = 0
+  vids = []
+
+  dl_cls_by_filter = str(dl_cls_by_filter)
+
+  pickleFile = 'parse_annotations.pickle'
+  pickleObj = {}
+  if os.path.exists( pickleFile ):
+    with open(pickleFile, 'rb') as handle:
+      pickleObj = pickle.load(handle) 
 
   d_set_dir = dl_dir+'/'+d_set+'/'
 
@@ -201,94 +216,319 @@ def parse_annotations(d_set,dl_dir,dl_cls_by_filter=-1):
   print (d_set+': Parsing annotations into clip data...')
 
   # Parse csv data.
-  annotations = []
-  with open((d_set+'.csv'), 'rt') as f:
-    reader = csv.reader(f)
-    annotations = list(reader)
+  if not read_csv_with_alternate_reader:
+    annotations = []
+    with open((d_set+'.csv'), 'rt') as f:
+      reader = csv.reader(f)
+      annotations = list(reader)
 
-  # Sort to de-interleave the annotations for easier parsing. We use
-  # `int(l[1])` to sort by the timestamps numerically; the other fields are
-  # sorted lexicographically as strings.
-  print(d_set + ': Sorting annotations...')
-  if ('classification' in d_set):
-    class_or_det = 'class'
-    # Sort by youtube_id, class, and then timestamp
-    annotations.sort(key=lambda l: (l[0], l[2], int(l[1])))
-  elif ('detection' in d_set):
-    class_or_det = 'det'
-    # Sort by youtube_id, class, obj_id and then timestamp
-    annotations.sort(key=lambda l: (l[0], l[2], l[4], int(l[1])))
+    # Sort to de-interleave the annotations for easier parsing. We use
+    # `int(l[1])` to sort by the timestamps numerically; the other fields are
+    # sorted lexicographically as strings.
+    print(d_set + ': Sorting annotations...')
+    if ('classification' in d_set):
+      class_or_det = 'class'
+      # Sort by youtube_id, class, and then timestamp
+      annotations.sort(key=lambda l: (l[0], l[2], int(l[1])))
+    elif ('detection' in d_set):
+      class_or_det = 'det'
+      # Sort by youtube_id, class, obj_id and then timestamp
+      annotations.sort(key=lambda l: (l[0], l[2], l[4], int(l[1])))
 
-  current_clip_name = ['blank']
-  clips             = []
+    current_clip_name = ['blank']
+    clips             = []
 
-  # Parse annotations into list of clips with names, youtube ids, start
-  # times and stop times
-  for idx, annotation in enumerate(annotations):
-    # If this is for a classify dataset there is no object id
-    if (class_or_det == 'class'):
-      obj_id = '0'
-    elif (class_or_det == 'det'):
-      obj_id = annotation[4]
-    yt_id    = annotation[0]
-    class_id = annotation[2]
+    # Parse annotations into list of clips with names, youtube ids, start
+    # times and stop times
+    for idx, annotation in enumerate(annotations):
+      # If this is for a classify dataset there is no object id
+      if (class_or_det == 'class'):
+        obj_id = '0'
+      elif (class_or_det == 'det'):
+        obj_id = annotation[4]
+      yt_id    = annotation[0]
+      class_id = annotation[2]
 
-    #added filter to download only specified class in the filter 
-    if not dl_cls_by_filter == -1 and not (int(dl_cls_by_filter)+1) == (int(class_id)+1):
-      continue
+      #added filter to download only specified class in the filter 
+      if not dl_cls_by_filter == -1 and not (int(dl_cls_by_filter)+1) == (int(class_id)+1):
+        continue
 
-    clip_name = yt_id+'+'+class_id+'+'+obj_id
+      clip_name = yt_id+'+'+class_id+'+'+obj_id
 
-    # If this is a new clip
-    if clip_name != current_clip_name:
+      # If this is a new clip
+      if clip_name != current_clip_name:
 
-      # Update the finishing clip
-      if idx != 0: # If this isnt the first clip
-        clips[-1].stop = annotations[idx-1][1]
+        # Update the finishing clip
+        if idx != 0: # If this isnt the first clip
+          clips[-1].stop = annotations[idx-1][1]
 
-      # Add the starting clip
-      clip_start = annotation[1]
-      clips.append( video_clip( \
-        clip_name, \
-        yt_id, \
-        clip_start, \
-        '0', \
-        class_id, \
-        obj_id, \
-        d_set_dir) )
+        # Add the starting clip
+        clip_start = annotation[1]
+        clips.append( video_clip( \
+          clip_name, \
+          yt_id, \
+          clip_start, \
+          '0', \
+          class_id, \
+          obj_id, \
+          d_set_dir) )
 
-      # Update the current clip name
-      current_clip_name = clip_name
+        # Update the current clip name
+        current_clip_name = clip_name
 
-  # Update the final clip with its stop time
-  clips[-1].stop = annotations[-1][1]
+    # Update the final clip with its stop time
+    clips[-1].stop = annotations[-1][1]
 
-  # Sort the clips by youtube id
-  clips.sort(key=lambda x: x.yt_id)
+    # Sort the clips by youtube id
+    clips.sort(key=lambda x: x.yt_id)
 
-  # Create list of videos to download (possibility of multiple clips
-  # from one video)
-  current_vid_id = ['blank']
-  vids = []
-  for clip in clips:
+    # Create list of videos to download (possibility of multiple clips
+    # from one video)
+    current_vid_id = ['blank']
+    vids = []
+    for clip in clips:
 
-    vid_id = clip.yt_id
+      vid_id = clip.yt_id
 
-    # If this is a new video
-    if vid_id != current_vid_id:
-      # Add the new video with its first clip
-      vids.append( video ( \
-        clip.yt_id, \
-        clip ) )
-    # If this is a new clip for the same video
+      # If this is a new video
+      if vid_id != current_vid_id:
+        # Add the new video with its first clip
+        vids.append( video ( \
+          clip.yt_id, \
+          clip ) )
+      # If this is a new clip for the same video
+      else:
+        # Add the new clip to the video
+        vids[-1].clips.append(clip)
+
+      # Update the current video name
+      current_vid_id = vid_id
+  else:
+    # annotations = []
+    # with open((d_set+'.csv'), 'rt') as f:
+    #   reader = csv.reader(f)
+    #   annotations = list(reader)
+
+    df = None 
+    chunksize = 2000
+
+    # 
+    if ('classification' in d_set):
+      class_or_det = 'class'
+    elif ('detection' in d_set):
+      class_or_det = 'det'
+
+    #create a sorted file first if it isn't exist
+    filesorted = d_set+'_'+dl_cls_by_filter+'_sorted.csv'
+    if not os.path.exists(filesorted):  
+      print(d_set + ': Sorting annotations...')
+
+      if not dl_cls_by_filter == -1:
+        # iter_csv = pd.read_csv(d_set+'.csv', iterator=True, chunksize=chunksize)
+        # fstr = dl_cls_by_filter   # str(dl_cls_by_filter-1)
+        # print( "fstr", fstr )
+        # df = pd.concat([chunk[ (chunk[chunk.columns[2]] == fstr) ] for chunk in iter_csv])
+
+        fcnt = -1
+        ccnt = -1
+        fval = int(dl_cls_by_filter) + 1
+        is_header_created = False
+        coll = 0
+        with open( filesorted, 'wb' ) as file:
+          for chunk in pd.read_csv(d_set+'.csv', header=None, dtype=str, chunksize=chunksize):
+            # # if True or df is None:
+            # if not is_header_created:
+            #   # df = pd.DataFrame(columns=chunk.columns) #['lib', 'qty1', 'qty2'])
+            #   is_header_created = True
+            #   line = ""
+            #   print( type(chunk.columns), chunk.columns)
+            #   coll = len(chunk.columns)
+            #   for colnm in chunk.columns:
+            #     line += "\""+colnm+"\","
+            #   file.write(line.encode())
+            #   file.write('\n'.encode())
+
+            ccnt += 1
+
+            for idx, annotation in chunk.iterrows():
+              # print( "columns", chunk.columns, chunk.columns[2] )
+              # print( "annotation", annotation )
+              if int(annotation[chunk.columns[2]])+1 == fval:
+                # fcnt += 1
+                # df.loc[fcnt] = annotation
+                line = ""
+                for colnm in chunk.columns:
+                  line += "\""+annotation[colnm]+"\","
+                  # print( "colnm ", colnm, annotation[colnm] )
+                file.write(line.encode())
+                file.write('\n'.encode())
+
+            print("chunk ", ccnt)
+
+        #
+        df = pd.read_csv(filesorted, header=None, dtype=str)
+
+        print( "df", len(df.index), fcnt )
+      else:
+        #load entire csv file in dataframe in df var
+        df = pd.read_csv(d_set+'.csv', header=None, dtype=str)
+
+      print("sorting the df frame")
+
+      #
+      df = df.astype({df.columns[1]: int})
+
+      #sort 
+      # Sort to de-interleave the annotations for easier parsing. We use
+      # `int(l[1])` to sort by the timestamps numerically; the other fields are
+      # sorted lexicographically as strings.
+      if ('classification' in d_set):
+        # class_or_det = 'class'
+        # Sort by youtube_id, class, and then timestamp
+        # annotations.sort(key=lambda l: (l[0], l[2], int(l[1])))
+        df.sort_values([df.columns[0], df.columns[2], df.columns[1]], ascending=[True, True, True])
+      elif ('detection' in d_set):
+        # class_or_det = 'det'
+        # Sort by youtube_id, class, obj_id and then timestamp
+        # annotations.sort(key=lambda l: (l[0], l[2], l[4], int(l[1])))
+        df.sort_values([df.columns[0], df.columns[2], df.columns[4], df.columns[1]], ascending=[True, True, True, True])
+
+      #
+      pickleObj[d_set+"_"+dl_cls_by_filter+"_totr"] = len(df.index)
+
+      #write sorted file
+      df.to_csv ( filesorted, index = False, header=True )
+
+      #
+      with open(pickleFile, 'wb') as handle:
+        pickle.dump(pickleObj, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
     else:
-      # Add the new clip to the video
-      vids[-1].clips.append(clip)
+      print(d_set + ': Sorted annotations file found...')
 
-    # Update the current video name
-    current_vid_id = vid_id
+    #
+    print("loading sorted csv file and creating clips")
 
-  return annotations,clips,vids
+    rig = pickleObj[d_set+"_"+dl_cls_by_filter+"_totr"]
+
+    #create dataframe of only applicable offset range 
+    if not offset_min == -1:
+      #offset logic, need to move down in clips/vids object loop
+      # df = pd.read_csv( filesorted, skiprows=offset_min, nrows=offset_max )
+
+      df = None   #read in chunks instead pd.read_csv(filesorted, header=None, dtype=str)
+    else:
+      #load entire csv file in dataframe in df var
+      df = None   #read in chunks instead pd.read_csv(filesorted, header=None, dtype=str)
+
+    current_clip_name = ['blank']
+    current_yt_id = ['blank']
+    clips             = []
+    vids_fcnt = 0
+    vid_pcidx = -1
+    ccnt = -1
+
+    # Parse annotations into list of clips with names, youtube ids, start
+    # times and stop times
+    # for idx, annotation in enumerate(annotations):
+    break_loop = False
+    for df in pd.read_csv(filesorted, header=None, dtype=str, chunksize=chunksize):
+      ccnt += 1
+      print("chunk ", ccnt)
+
+      for idx, annotation in df.iterrows():
+        
+        # If this is for a classify dataset there is no object id
+        if (class_or_det == 'class'):
+          obj_id = '0'
+        elif (class_or_det == 'det'):
+          obj_id = annotation[df.columns[4]] #[4]
+        yt_id    = annotation[df.columns[0]] #[0]
+        class_id = annotation[df.columns[2]] #[2]
+
+        # #added filter to download only specified class in the filter 
+        # if not dl_cls_by_filter == -1 and not (int(dl_cls_by_filter)+1) == (int(class_id)+1):
+        #   continue
+
+        clip_name = yt_id+'+'+class_id+'+'+obj_id
+
+        # If this is a new clip
+        if clip_name != current_clip_name:
+          if yt_id != current_yt_id:
+            current_yt_id = yt_id
+            vids_fcnt += 1
+
+          if vids_fcnt >= offset_min and vids_fcnt <= offset_max:
+            vid_pcidx += 1
+
+            # Update the finishing clip
+            if vid_pcidx != 0: # idx != 0: # If this isnt the first clip
+              clips[-1].stop = df[idx-1:idx][df.columns[1]]  #annotations[idx-1][1]
+
+            # Add the starting clip
+            clip_start = annotation[df.columns[1]]  #[1]
+            clips.append( video_clip( \
+              clip_name, \
+              yt_id, \
+              clip_start, \
+              '0', \
+              class_id, \
+              obj_id, \
+              d_set_dir) )
+
+          elif vids_fcnt > offset_max:
+            break_loop = True
+
+          # Update the current clip name
+          current_clip_name = clip_name
+
+        if break_loop:
+          break
+
+      if break_loop:
+        break
+
+    # Update the final clip with its stop time
+    if not break_loop:
+      lind = len(df.index)
+      clips[-1].stop = df[lind-1:lind][df.columns[1]]  #annotations[-1][1]
+
+    #
+    print("sorting clips")
+
+    # Sort the clips by youtube id
+    clips.sort(key=lambda x: x.yt_id)
+
+    #
+    print("looping through clips and creating vid objects")
+
+    # Create list of videos to download (possibility of multiple clips
+    # from one video)
+    current_vid_id = ['blank']
+    for clip in clips:
+
+      vid_id = clip.yt_id
+
+      # If this is a new video
+      if vid_id != current_vid_id:
+        # Add the new video with its first clip
+        vids.append( video ( \
+          clip.yt_id, \
+          clip ) )
+      # If this is a new clip for the same video
+      else:
+        # Add the new clip to the video
+        vids[-1].clips.append(clip)
+
+      # Update the current video name
+      current_vid_id = vid_id
+
+  #
+  with open(pickleFile, 'wb') as handle:
+    pickle.dump(pickleObj, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+  #return annotations,clips,vids, rig
+  return None,None,vids, rig
 
 def sched_downloads(d_set,dl_dir,num_threads,vids,rec_ind=-1,offset_min=-1,offset_max=-1,FREE_SPACE_LIMIT=-1):
   d_set_dir = dl_dir+'/'+d_set+'/'
